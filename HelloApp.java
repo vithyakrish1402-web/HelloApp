bfc5f47694455ccf8712a5ec60d14e106f39f426
Vithya Krishnan R A <vithyakrish1402@gmail.com>
2026-03-11 10:19:26 +0530

    done

---
+++ HelloApp.java
@@ -0,0 +1,12 @@
+/**
+ * HelloApp.java – A simple Java application that displays "Hello, World!" 
+ * to the console.
+ */
+
+public class HelloApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
